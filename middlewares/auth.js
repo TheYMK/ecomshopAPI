609272b8071ad02263a5b791111c21e257765e9a
Kaym Kassai <kaymkassai269@gmail.com>
2020-11-04 12:18:51 +0300

    User Admin and Protected Routes

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,5 @@
 const admin = require('../firebase');
+const User = require('../models/user');
 
 exports.authCheck = async (req, res, next) => {
 	try {
@@ -11,5 +12,20 @@ exports.authCheck = async (req, res, next) => {
 		res.status(401).json({
 			error: 'Invalid or expired token'
 		});
+	}
+};
+
+// the above middleware will be apply before this one. So we will already have the user under req.user
+exports.adminCheck = async (req, res, next) => {
+	const { email } = req.user;
+
+	const adminUser = await await User.findOne({ email: email });
+
+	if (adminUser.role !== 'admin') {
+		res.status(403).json({
+			err: 'Admin resource. Access denied'
+		});
+	} else {
+		next();
 	}
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,13 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 // middlewares
-const { authCheck } = require('../../middlewares/auth');
+const { authCheck, adminCheck } = require('../../middlewares/auth');
 // controllers
 const { createOrUpdateUser, currentUser } = require('../../controllers/auth');
 
+// USER ROUTES
 router.post('/create-or-update-user', authCheck, createOrUpdateUser);
-
 // post request so that people don't make unnecessary requests. Since get requests are easy to make
 router.post('/current-user', authCheck, currentUser);
+
+// ADMIN ROUTES
+router.post('/current-admin', authCheck, adminCheck, currentUser);
 
 module.exports = router;
